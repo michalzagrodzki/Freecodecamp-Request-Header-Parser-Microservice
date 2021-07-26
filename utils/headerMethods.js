8e67ef6576ab1736f2e88ed3c4888cac79442d5f
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-26 16:37:59 +0200

    Add method to determine language from request

--- utils/headerMethods.js
+++ utils/headerMethods.js
@@ -6,8 +6,13 @@ determineIpAddress = (request) => {
   if (request.ip) return request.ip;
   throw "IP in request not present";
 };
-determineLanguage = () => {
-  return;
+determineLanguage = (request) => {
+  if (request.headers["accept-language"]) {
+    const languages = request.headers["accept-language"].split(",");
+    const response = `${languages[0]},${languages[1]}`;
+    return response;
+  }
+  throw "Languages in request not present";
 };
 determineSoftware = (request) => {
   if (request.headers["user-agent"]) return request.headers["user-agent"];
